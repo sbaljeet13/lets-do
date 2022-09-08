5e4c261e30c5aa2d73cd94443c5e8fb3eb499a7a
Baljeet Singh <22825252+sbaljeet13@users.noreply.github.com>
2022-09-07 23:11:58 -0700

    Add Emojis for Buttons
    Add Emojis for Complete, Incomplete and Delete Buttons

--- src/Task.js
+++ src/Task.js
@@ -6,21 +6,25 @@ export default function Task({id, title, isComplete, isDeleted }) {
     const [complete, setComplete] = useState(isComplete);
     const [deleted, setDeleted] = useState(isDeleted);
 
-    function addClasses (complete, deleted) {
-        let classes = complete ? ' complete ' : ' incomplete ';
-        classes += deleted ? ' deleted ' : ' not-deleted ';
+    function addClasses (deleted) {
+        let classes = deleted ? ' deleted ' : ' not-deleted ';
 
         return classes;
     }
 
     return (
         <li key={id}>
-            <span className={addClasses(complete, deleted)}
-                onClick={() => setComplete(!complete)}>
+            <button onClick={() => setComplete(!complete)}>
+                {complete ? '\u2716' : '\u26AB'}
+            </button>
+            
+            <span className={addClasses(deleted)}>
                 {title}
             </span>
             
-            <button onClick={() => setDeleted(!deleted)}>x</button>
+            <button onClick={() => setDeleted(!deleted)}>
+                {'\u274C'}
+            </button>
         </li>
     );
 }
